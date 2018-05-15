28926b3d04d60ad87b1f438a9686f13c64be8be7
pavel-else <de.pavel.se@yande.ru>
2018-05-16 01:48:51 +0300

    Добавлена функция инициализации слагаемых

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 new Vue({
 		el: '#app',
   	data: {
-    	message: 'Привет, Vue!'
+    	a: '333',
+    	b: ''
+  	},
+  	methods: {
+  		initial(){
+	  		// init a, b
+	  		// a [6,9], a+b[11,14]
+
+	  		this.a = Math.floor(Math.random() * 4 + 6);
+	  		this.b = Math.floor(Math.random() * 4 + (11 - this.a));	
+  		}	
+  	},
+  	beforeMount(){
+  		this.initial();
   	}
 })
