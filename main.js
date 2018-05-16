03838c49a220dcd6f28de6072b2fa11a55d9f34c
pavel-else <de.pavel.se@yande.ru>
2018-05-16 14:24:10 +0300

    Рефакторинг кода

--- main.js
+++ main.js
@@ -1,55 +1,89 @@
 new Vue({
 		el: '#app',
   	data: {
-    	a: '333',
-    	b: '',
-
     	l: 38.85,
-    	arrowWidth1: 0,
-    	arrowWidth2: 0,
-    	arrowInputErrorA: false,
-    	arrowInputErrorB: false,
 
-    	step1: true,
-    	step2: false,
-    	step3: false
+      arrows: {
+        a: {
+          value: 0,
+          width: 0,
+          inputError: false,
+          inputBlock: false,
+          show: true,
+          focus: false,
+        },
+        b: {
+          value: 0,
+          width: 0,
+          inputError: false,
+          inputBlock: false,
+          show: false,
+          focus: false,
+        }
+      },
+      caption: {
+        result: {
+          focus: false,
+          disabled: false,
+          value: '?',
+          focus: true,
+        }
+      }
   	},
   	methods: {
   		initial(){
 	  		// init a, b
 	  		// a [6,9], a+b[11,14]
 
-	  		this.a = Math.floor(Math.random() * 4 + 6);
-	  		this.b = Math.floor(Math.random() * 4 + (11 - this.a));	
 
-	  		this.arrowWidth1 = this.a * this.l;
-	  		this.arrowWidth2 = this.b * this.l;
+	  		this.arrows.a.value = Math.floor(Math.random() * 4 + 6);
+        this.arrows.b.value = Math.floor(Math.random() * 4 + (11 - this.arrows.a.value)); 
+
+        this.arrows.a.width = this.arrows.a.value * this.l;
+	  		this.arrows.b.width = this.arrows.b.value * this.l;
+
+        this.caption.result.inputBlock = true;
+        this.arrows.a.focus = true;
   		},
   		arrowInputA(val) {
-  			if (val == this.a) {
-  				this.step1 = false;
-  				this.step2 = true;
-  				this.arrowInputErrorA = false;
-  			} else {
-  				this.arrowInputErrorA = true;
+  			if (val == this.arrows.a.value) {
+          this.arrows.a.inputError = false;
+          this.arrows.a.inputBlock = true;
+          this.arrows.b.show = true;
+          this.arrows.b.focus = true;
+        } else {
+          this.arrows.a.inputError = true;
+          this.arrows.a.focus = false;
+          this.arrows.b.inputBlock = false;
   			}
   		},
   		arrowInputB(val) {
-  			if (val == this.b) {
-   				this.step3 = true;
-   				this.arrowInputErrorB = false;
-  			} else {
-  				this.arrowInputErrorB = true;
-  			}
+        if (val == this.arrows.b.value) {
+          this.arrows.b.inputError = false;
+          this.arrows.b.inputBlock = true;
+          this.arrows.b.focus = false;
+
+          this.caption.result.value = '';
+          this.caption.result.inputBlock = false;
+          this.caption.result.focus = true;
+        } else {
+          this.arrows.b.inputError = true;
+          this.arrows.b.inputBlock = false;
+        }
   		}
-
-
   	},
   	beforeMount() {
   		this.initial();
   	},
   	mounted() {
   		// console.log(this.arrowWidth1)
-  	}
-
+  	},
+    directives: {
+      focus: {
+        // определение директивы
+        inserted: function (el, binding) {
+          if (binding.value.active) el.focus()
+        }
+      }
+    }
 })
